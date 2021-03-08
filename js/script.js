617d55ab9005e0efaf941666bbdc3393a4882bbd
maxinflame <maxinflame@yandex.ru>
2021-03-09 00:30:49 +0300

    Оживление интерфейса

--- js/script.js
+++ js/script.js
@@ -4,10 +4,22 @@ const dateIn = form.querySelector('[name=date-in]');
 const dateOut = form.querySelector('[name=date-out]');
 const adults = form.querySelector('[name=adults]');
 const children = form.querySelector('[name=children]');
+const searchButton = form.querySelector('.search-button');
 
+let isStorageSupport = true;
+let storageAdults = "";
+let storageChildren = "";
+
+try {
+  storageAdults = localStorage.getItem('adults');
+  storageChildren = localStorage.getItem('children');
+} catch (err) {
+  isStorageSupport = false;
+}
 
 openButton.addEventListener('click', function(evt){
   evt.preventDefault();
+  form.classList.remove('form-error');
   form.classList.toggle('show-form');
   form.classList.toggle('hidden-form');
 })
@@ -16,11 +28,28 @@ openButton.addEventListener('mousedown', function(evt){
     evt.preventDefault()
 });
 
+searchButton.addEventListener('mousedown', function(evt){
+    evt.preventDefault()
+});
+
+if(storageAdults) {
+  adults.value = storageAdults;
+}
+
+if (storageChildren) {
+  children.value = storageChildren;
+}
+
 form.addEventListener('submit', function(evt) {
   if(!dateIn.value || !dateOut.value || !adults.value || !children.value) {
     evt.preventDefault();
-    form.classList.add('form-error');
-    form.offsetWidth = form.offsetWidth;
     form.classList.remove('form-error');
-  }
+    form.offsetWidth = form.offsetWidth;
+    form.classList.add('form-error');
+  } else {
+      if (isStorageSupport) {
+        localStorage.setItem('adults', adults.value);
+        localStorage.setItem('children', children.value);
+      }
+    }
 });
